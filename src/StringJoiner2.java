a1a210a916a4a9ef09e3608e34dfd11ec2ec5b44
baker busulwa <bbusulwa58@gmail.com>
2024-10-01 23:05:28 -0700

    new changes

--- src/StringJoiner2.java
+++ src/StringJoiner2.java
@@ -23,8 +23,18 @@ summing = A.compareTo(B); //System.out.println(summing);  // A is smaller than B
         }else
             System.out.println("Yes") ;
 
-System.out.println(A.toUpperCase().charAt(0)+ "ello "+" " +B.toUpperCase().charAt(0)+"ava");
+System.out.println(A.toUpperCase().charAt(0)+ "ello "+" " +B.toUpperCase().charAt(0)+"ava\n\n");
+
+//*****************************************************************************
+System.out.print("Working with substrings\n");
+        //Sub Strings
+        String sub1= "Hello World";
+        String sub2= "Helloworld";
+        String str= sub1.substring(7);
+   String str2= sub2.substring(3,7);
+        System.out.println(str);
 
 
+        System.out.println(str2);
     }
 }
